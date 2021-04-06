e41b39db4c1cdc33c9d36d092c90497f36e51446
uwin <fb.uwin@gmail.com>
2021-04-06 17:16:33 +0530

    Update Flow.java

--- src/cw/Flow.java
+++ src/cw/Flow.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Flow {
@@ -12,44 +13,128 @@ public class Flow {
     static int sink;
     static int source;
     static int edges;
-    static long start; //starting time
+    static long start;
     static ArrayList<String> flow = new ArrayList<String>();
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main (String[] args) throws java.lang.Exception {
         Flow m = new Flow();
-
+        while (true) m.runalgo();
+    }
+    void runalgo(){
+        Flow m = new Flow();
         Scanner input = new Scanner(System.in);
-        int choice=0;
-        int nodes=0;
+        String choice;
+        System.out.println("1. Load graphs\n2. exit");
+        choice = input.next();
+        switch (choice) {
+            case "1":
+                m.LoadingOtherGraphs(m,input);
+                m.Display(m);
+                break;
+            case "2":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Input");
+                runalgo();
+                break;
+        }
+    }
 
-        while(choice!=2) {
-            System.out.println("1. Load graphs\n2. exit");
-            choice = input.nextInt();
-            switch (choice) {
-                case 1:
-                    m.LoadingOtherGraphs(m,input);
-                    m.Display(m);
-                    break;
-                case 2:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid Input");
-                    break;
+    boolean bfs(int rGraph[][], int s, int t, int parent[],int nodes) {
+        // Create a visited array and mark all vertices as not
+        // visited
+        boolean visited[] = new boolean[nodes];
+        for(int i=0; i<nodes; ++i)
+            visited[i]=false;
+
+        // Create a queue, enqueue source vertex and mark
+        // source vertex as visited
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.add(s);
+        visited[s] = true;
+        parent[s]=-1;
+
+        // Standard BFS Loop
+        while (!queue.isEmpty())
+        {
+            int u = queue.poll();
+            if (s==t) break;
+            for (int v=0; v<nodes; v++)
+            {
+                if (visited[v]==false && rGraph[u][v] > 0)
+                {
+                    queue.add(v);
+                    parent[v] = u;
+                    visited[v] = true;
+                }
             }
         }
-
+        // If we reached sink in BFS starting from source, then
+        // return true, else false
+        return (visited[t] == true);
     }
+    int fordFulkerson(int graph[][],int nodes) {
+
+        int u, v;
+        // Create a residual graph and fill the residual graph
+        // with given capacities in the original graph as
+        // residual capacities in residual graph
+
+        // Residual graph where rGraph[i][j] indicates
+        // residual capacity of edge from i to j (if there
+        // is an edge. If rGraph[i][j] is 0, then there is
+        // not)
+        int rGraph[][] = new int[nodes][nodes];
+
+
+        for (u = 0; u < nodes; u++)
+            for (v = 0; v < nodes; v++)
+                rGraph[u][v] = graph[u][v];
+
+        // This array is filled by BFS and to store path
+        int parent[] = new int[nodes];
+
+        int max_flow = 0; // There is no flow initially
+
+        // Augment the flow while there is path from source to sink
+        while (bfs(rGraph, source, sink, parent,nodes))
+        {
+//            System.out.println("in ford while");
+            // Find minimum residual capacity of the nodes along the path filled by BFS. Or we can say
+            // find the maximum flow through the path found.
+            int path_flow = Integer.MAX_VALUE;
+            for (v=sink; v!=source; v=parent[v])
+            {
+                u = parent[v];
+                path_flow = Math.min(path_flow, rGraph[u][v]);
+            }
+
+            // update residual capacities of the edges and
+            // reverse edges along the path
+            for (v=sink; v != source; v=parent[v])
+            {
+                u = parent[v];
+                rGraph[u][v] -= path_flow;
+                rGraph[v][u] += path_flow;
+                flow.add(u+"-->"+v+" ");
+            }
+            // Add path flow to overall flow
+            max_flow += path_flow;
+        }
+        // Return the overall flow
+        return max_flow;
+    }
+
     void LoadingOtherGraphs(Flow m, Scanner input){
-        String number="";
+        int count=0;
         String filetype="";
         String filename="";
-        int count=0;
-        int count2=0;
-        int col=0;
         edges=0;
         int choice=0;
+//        String number="";
+//        int count2=0;
+//        int col=0;
         System.out.println("1. Bridge\n2. Ladder");
         choice=input.nextInt();
         switch (choice){
@@ -100,21 +185,24 @@ public class Flow {
         }
 
         start=System.currentTimeMillis();
+
         try {
             File myObj = new File("G:\\My Drive\\iit\\lvl05\\SEM2\\Algo\\CW\\code\\src\\Some input files for testing\\"+filename+".txt");
             Scanner myReader = new Scanner(myObj);
             String data = myReader.nextLine();
             snodes= Integer.parseInt(data.trim());
+            source= 0;
+            sink=snodes-1;
             count++;
 
             graph = new int[snodes][snodes];
-            if(snodes>0){
-                for (int i = 0; i < snodes; i++) {
-                    for (int j = 0; j < snodes; j++) {
-                        graph[i][j]=0;
-                    }
-                }
-            }
+//            if(snodes>0){
+//                for (int i = 0; i < snodes; i++) {
+//                    for (int j = 0; j < snodes; j++) {
+//                        graph[i][j]=0;
+//                    }
+//                }
+//            }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
@@ -127,28 +215,15 @@ public class Flow {
             count=0;
             while (myReader2.hasNextLine()) {
                 String data2 = myReader2.nextLine();
-                if(count>1){
-                    System.out.println("graph = " + Arrays.deepToString(graph));
+                if(count>0){
+                    String[] arrayString = data2.split(" ");
+                    int StartNode = Integer.parseInt(arrayString[0]);
+                    int EndNode = Integer.parseInt(arrayString[1]);
+                    int capacity = Integer.parseInt(arrayString[2]);
+                    edges++;
+                    graph[StartNode][EndNode] = capacity;
                 }
                 count++;
-//                    number="";
-//                    while(data2.length()!=count2) {
-//                        if (data2.substring(count2, count2+1).equals(",")) {
-////                            graph[(count-3)][col]=Integer.parseInt(number);
-//                            if(Integer.parseInt(number)>0){
-//                                edges++;
-//                            }
-//                            col++;
-//                            number="";
-//                        }else{
-//                            number=number+data2.substring(count2,count2+1);
-//                        }
-//                        count2++;
-//
-//                    }
-//                }
-//                col=0;
-//                count2=0;
             }
             myReader2.close();
         } catch (FileNotFoundException e) {
@@ -158,20 +233,16 @@ public class Flow {
     }
     void Display(Flow m){
         int count=0;
-        System.out.println("GRAPH VIEW ");
-        if(snodes>0){
-            for (int i = 0; i < snodes; i++) {
-                for (int j = 0; j < snodes; j++) {
-                    System.out.print(graph[i][j]+" ");
-                }
-                System.out.println();
-            }
-        }
-        System.out.println("Edges : "+edges);
-        System.out.println("Nodes : "+snodes);
-//        System.out.println("Max flow " + m.fordFulkerson(graph, snodes));
-        long end=System.currentTimeMillis();
-        System.out.println("Time : "+(end-start)+"ms");
+//        System.out.println("GRAPH VIEW ");
+//        if(snodes>0){
+//            for (int i = 0; i < snodes; i++) {
+//                for (int j = 0; j < snodes; j++) {
+//                    System.out.print(graph[i][j]+" ");
+//                }
+//                System.out.println();
+//            }
+//        }
+        int maxflow=  m.fordFulkerson(graph, snodes);
         count=flow.size();
         System.out.println("AUGMENTED PATH");
         for(int i=0;i<flow.size();i++){
@@ -187,5 +258,13 @@ public class Flow {
             }
         }
         System.out.println();
+        long end=System.currentTimeMillis();
+
+        System.out.println("Max flow : " +maxflow);
+        System.out.println("Edges : "+edges);
+        System.out.println("Nodes : "+snodes);
+        System.out.println();
+        System.out.println("Time : "+(end-start)+"ms");
+
     }
 }
